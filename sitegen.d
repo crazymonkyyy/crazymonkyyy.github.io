df909f34f0b6afb0b53a8815fa9a6aa464123ec4
crazymonkyyy <crazymonkyyy@gmail.com>
2023-07-17 18:46:16 +0000

    updated script

--- sitegen.d
+++ sitegen.d
@@ -2,7 +2,23 @@
 enum siteconfig="config";
 import std;
 void main(){
-	foreach(s;File(siteconfig).byLine){
+	foreach(s;File(siteconfig).byLineCopy){
 		executeShell("rm "~s~".html");
-		executeShell("cat header.html "~s~" footer.html > "~s~".html");
-}}
+		auto o=File(s~".html","w");
+		//executeShell("cat header.html "~s~" footer.html > "~s~".html");
+		foreach(t;File("header.html").byLineCopy){
+			o.writeln(t);
+		}
+		foreach(t;File(s).byLineCopy){
+			if(t==""){
+				o.writeln("<p>");
+			} else {
+				o.writeln(t);
+			}
+		}
+		foreach(t;File("footer.html").byLineCopy){
+			o.writeln(t);
+		}
+	}
+	//executeShell("python -m http.server");
+}
